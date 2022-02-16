feeca1037f38c649a4ea41aaefb58c23c3a0733f
Tarek-khwaiss <tarekonguitar@hotmail.com>
2022-02-16 23:02:59 +0200

    variables tackled

--- bin/variables.dart
+++ bin/variables.dart
@@ -1,3 +1,28 @@
 void main(){
+
+  int value = 3;
+  bool isTrue = false;
+
+  int? age;
+  age = 3;
+  print (age);
   
+  Object x = animal();
+  x.toString;
+
+
+  print("$value is a $isTrue number");
+
 }
+
+class animal{
+
+  // here late keyword is used to assign the final value later
+  late final String size;
+
+  void goBig(){
+    size = "huge";
+    print(size);
+  }
+}
+
